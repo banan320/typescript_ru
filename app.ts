fef1bd98e497d52ee164f14c7330181a9f484121
Alex Gerber <alex-gerber@mail.ru>
2023-05-09 12:26:45 +0300

    Optional

--- app.ts
+++ app.ts
@@ -213,3 +213,36 @@
 // interface IDI {
 //   ID: string,
 // }
+
+//* Optional  ===============
+// interface User {
+//   login: string;
+//   password?: string;
+// }
+// type User2 = {
+//   login: string;
+//   password?: string;
+// }
+
+// const user: User = {
+//   login: "ss@ss.ru",
+// };
+
+// function multiply(first: number, second?: number ): number {
+//   if(!second) {
+//     return first * first
+//   }
+//   return first * second;
+// }
+// interface UserPro {
+//   login: string;
+//   password?: {
+//     type: "primary" | "secondary"
+//   };
+// }
+// function testPass(user: UserPro) {
+//   const t = user.password?.type
+// }
+// function test(param?: string) {
+//   const t = param ?? multiply(5)
+// }
