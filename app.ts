5d47421fe627789ca95f1c0e501efdd98494335a
Alex Gerber <alex-gerber@mail.ru>
2023-05-09 15:25:48 +0300

    Void, test 3

--- app.ts
+++ app.ts
@@ -246,3 +246,16 @@
 // function test(param?: string) {
 //   const t = param ?? multiply(5)
 // }
+
+//* Void  ===============
+// type voidFunc = () => void;
+// const f1: voidFunc = () => {};
+// const f2: voidFunc = () => {
+//   return true;
+// };
+// const b = f2();
+// const skills = ["Dev", "DevOps"];
+// const user = {
+//   s: ["s"],
+// };
+// skills.forEach((skill) => user.s.push(skill));
--- test.ts
+++ test.ts
@@ -35,3 +35,35 @@
 //   const data = await res.json();
 //   return data;
 // }
+
+// interface IPayment {
+//   sum: number;
+//   from: number;
+//   to: number;
+// }
+
+// enum PaymentStatus {
+//   Success = "success",
+//   Failed = "failed",
+// }
+
+// interface IPaymentRequest extends IPayment {}
+
+// interface IDataSuccess extends IPayment {
+//   databaseId: number;
+// }
+// interface IDataFailed {
+//   errorMessage: string;
+//   errorCode: number;
+// }
+
+// interface IResponseSuccess {
+//   status: PaymentStatus.Success;
+//   data: IDataSuccess;
+// }
+
+// interface IResponseSFailed {
+//   status: PaymentStatus.Failed;
+//   data: IDataFailed;
+// }
+// function get(): IResponseSuccess | IResponseSFailed {}
